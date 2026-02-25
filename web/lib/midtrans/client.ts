5a019c22893185b1b79ac6bf64eccd7722d8989e
agusetina-repo <agustina.repo@gmail.com>
2026-02-25 17:19:24 +0700

    clean install

--- web/lib/midtrans/client.ts
+++ web/lib/midtrans/client.ts
@@ -1,13 +1,13 @@
 import midtransClient from "midtrans-client";
 
 export const snap = new midtransClient.Snap({
-  isProduction: false, // Ganti true saat production
+  isProduction: false,
   serverKey: process.env.MIDTRANS_SERVER_KEY!,
   clientKey: process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY!,
 });
 
 export const coreApi = new midtransClient.CoreApi({
-  isProduction: false, // Ganti true saat production
+  isProduction: false,
   serverKey: process.env.MIDTRANS_SERVER_KEY!,
   clientKey: process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY!,
 });
